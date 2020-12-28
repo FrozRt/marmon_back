b9adf05e6f9703cf3e8ba1a3b17df8486a0e2727
Mikhail Chigrin <chigrin_m@mail.ru>
2020-12-28 03:43:00 +0300

    Set up database

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ from apps.stonks.controllers import stonks
 
 
 app = FastAPI(debug=DEBUG,
-              title='Simple-construction',
-              version='0.1-a',
+              title='Marmon',
+              version='0.1.1',
               docs_url='/docs')
 
 
@@ -26,7 +26,43 @@ async def startup():
     await database.connect()
 
 
-@app.on_event("startup")
-async def startup():
-    """Async support for connecting to database."""
-    await database.connect()
+@app.on_event("shutdown")
+async def shutdown():
+    """Async support for disconnect from database."""
+    await database.disconnect()
+
+
+@app.middleware("http")
+async def exception_middleware(request, call_next):
+    """Exception middleware, for handling exception."""
+    if not DEBUG:
+        try:
+            response = await call_next(request)
+        except Exception as error:
+            status_code = getattr(error, 'status_code', 500)
+            response = JSONResponse(content={'detail': 'Server error'}, status_code=status_code)
+        return response
+    else:
+        return await call_next(request)
+
+
+@app.middleware("http")
+async def cors_middleware(request: Request, call_next):
+    if request.method == 'OPTIONS':
+        response = Response(status_code=204)
+    else:
+        response = await call_next(request)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT, DELETE, HEAD, PATCH'
+    response.headers['Access-Control-Max-Age'] = '86400'
+    return response
+
+
+@app.middleware("http")
+async def process_time_middleware(request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
